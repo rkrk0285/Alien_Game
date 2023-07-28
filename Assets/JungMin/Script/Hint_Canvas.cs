4416ff90a5001b849369431140859021921caaee
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2023-07-28 21:44:57 +0900

    Hint Canvas

--- Assets/JungMin/Script/Hint_Canvas.cs
+++ Assets/JungMin/Script/Hint_Canvas.cs
@@ -8,21 +8,21 @@ public class Hint_Canvas : MonoBehaviour
     public string next_Scene;
     float time;    
 
-    private void OnEnable()
+    private void OnEnable() //힌트 캔버스가 활성화될 때, 코루틴 실행.
     {        
         time = 3f;
         StartCoroutine(Start_Timer());
     }
 
-    IEnumerator Start_Timer()
+    IEnumerator Start_Timer() 
     {
-        while (time >= 0.9f)
+        while (time >= 0f) // 3초간 힌트 창을 켜주고
         {
             time -= Time.deltaTime;
             yield return null;            
         }
-        gameObject.SetActive(false);
-        Load_Next();
+        gameObject.SetActive(false); // 힌트창 끄고
+        Load_Next(); // 다음 퀴즈로
         yield return null;
     }
 
